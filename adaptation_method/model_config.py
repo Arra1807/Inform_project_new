4075c566388d67eedb91f2d4fd258cab3b44af89
Arangan Subramaniam <142887982+Arra1807@users.noreply.github.com>
2025-07-28 19:10:33 +0200

    Update model_config.py

--- adaptation_method/model_config.py
+++ adaptation_method/model_config.py
@@ -1,12 +1,12 @@
 import wandb
-
-run = wandb.init(
-    project= 'Adapter', 
-    config = {
-        'model': 'ResNetAutoEncoder', 
-        'batch_size': 64,
-        'learning_rate': 1e-3,        
-        'epochs': 20,
-    }, 
-)
-
+def Configuration():
+    run = wandb.init(
+        project= 'Adapter', 
+        config = {
+            'model': 'ResNetAutoEncoder', 
+            'batch_size': 64,
+            'learning_rate': 1e-3,        
+            'epochs': 20,
+        }, 
+    )
+    return run
