b0eff49f2f157d54f6e43dab6e44da8cd089d3f9
Arangan Subramaniam <142887982+Arra1807@users.noreply.github.com>
2025-08-01 19:32:53 +0200

    Update model_config.py

--- adaptation_method/model_config.py
+++ adaptation_method/model_config.py
@@ -6,7 +6,7 @@ def Configuration(model_name = 'ResNetAutoEncoder'):
         config = { 
             'model': model_name
             'batch_size': 32,
-            'learning_rate': 3e-4,        
+            'learning_rate': 1e-5,        
             'epochs': 50,
             'Weight_decay': 1e-4
         }, 
