0fd312e6548b3eaa1a8890d5b2d3210226c5489f
Arangan Subramaniam <142887982+Arra1807@users.noreply.github.com>
2025-08-08 19:02:49 +0200

    Update model_config.py

--- adaptation_method/model_config.py
+++ adaptation_method/model_config.py
@@ -7,11 +7,12 @@ def Configuration(model_name = ''):
         config = {
             'batch_size': 32,
             'learning_rate': 1e-3,        
-            'epochs': 50,
+            'epochs': 70,
             'Weight_decay': 1e-4
         }, 
     )
     return run
+
 
 
 
